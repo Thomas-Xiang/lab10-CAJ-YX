ab2a85ebeef132d9d2621a9cb5de99da31cbebfc
Celion Celion <celioncelion@Mac.lan>
2025-04-12 12:31:38 -0700

    modified calculator p2

--- calculator.py
+++ calculator.py
@@ -20,14 +20,15 @@ def mul(a, b):
 def div(a, b):
     if a == 0:
         raise ZeroDivisionError
-    else:
-        return b / a
+    return b / a
     
 def log(a, b):
+    if a <= 0 or a==1 or b <=0:
+        raise ValueError("Invalid input for logarithm.")
     return math.log(b, a)
 
 def exp(a, b):
-    return a ^ b
+    return a ** b
 
 
 
